02462a64fdc8c9031884ed119d685b1e3128f09b
hyan.campos <hyan.campos@luizalabs.com>
2022-03-07 15:00:03 -0300

    feat: constant images

---
+++ src/constants/images.constant.ts
@@ -0,0 +1,25 @@
+import image10 from '../assets/images/10.jpeg';
+import image12 from '../assets/images/12.jpeg';
+import image13 from '../assets/images/13.jpeg';
+import image14 from '../assets/images/14.jpeg';
+import image16 from '../assets/images/16.jpeg';
+import image17 from '../assets/images/17.jpeg';
+import image19 from '../assets/images/19.jpeg';
+import image23 from '../assets/images/23.jpeg';
+import image24 from '../assets/images/24.jpeg';
+import image25 from '../assets/images/25.jpeg';
+
+const CardsImages = [
+  image10,
+  image12,
+  image13,
+  image14,
+  image16,
+  image17,
+  image19,
+  image23,
+  image24,
+  image25,
+];
+
+export default CardsImages;
