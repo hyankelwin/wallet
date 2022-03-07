0666ded8ef22e0fb11e9aaa42242ec5f165a5cc9
hyan.campos <hyan.campos@luizalabs.com>
2022-03-07 15:11:49 -0300

    feat: style color global

---
+++ src/styles/color.ts
@@ -0,0 +1,25 @@
+const Color = {
+  primary: {
+    default: '#252a40',
+    light: '#cecfff',
+  },
+  grey: '#c5c5c5',
+  white: '#FFFFFF',
+  white_opacity: '#FFF9',
+  success: {
+    default: '#20be79',
+    light: '#CCF7BB',
+  },
+  error: {
+    default: '#c53030',
+    light: '#ffd2d2',
+  },
+  alert: {
+    default: '#FFDC83',
+    dark: '#FB9600',
+  },
+  text: '#555555',
+  background: '#252a40',
+};
+
+export default Color;
