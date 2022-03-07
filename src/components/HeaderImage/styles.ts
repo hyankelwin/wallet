3236a77643edba840207d7866fcdb80fdecc7110
hyan.campos <hyan.campos@luizalabs.com>
2022-03-07 14:56:16 -0300

    feat: component headerImage

---
+++ src/components/HeaderImage/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import img from '../../assets/logos/logo.png';
+
+export const Content = styled.div`
+  height: 100px;
+  width: 200px;
+  background-size: contain;
+  margin-top: 20px;
+  background-image: url(${img});
+  background-size: contain;
+  background-repeat: no-repeat;
+`;
